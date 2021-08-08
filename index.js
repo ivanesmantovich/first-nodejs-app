623f34293e9c982c486016a1ea5b24b19c6a1650
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-08-08 21:26:37 +0700

    Start building a NodeJS app

---
+++ index.js
@@ -0,0 +1,6 @@
+import {express, response} from 'express';
+import { read, readFile } from 'fs';
+
+const app = express();
+
+git remote add origin https://github.com/lifemaintenance/first-nodejs-app.git
